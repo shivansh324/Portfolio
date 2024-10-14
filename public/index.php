f712abb6ac2e771e1f409a7ae4900a94638b9291
Shivansh Agarwal <shivansha56@gmail.com>
2024-10-14 18:57:49 +0530

    Vecel Deployment

--- public/index.php
+++ public/index.php
@@ -9,6 +9,8 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
     require $maintenance;
 }
 
+require __DIR__.'./style.css';
+
 // Register the Composer autoloader...
 require __DIR__.'/../vendor/autoload.php';
 
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,7 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <link rel="stylesheet" href="{{ url('style.css') }}">
+    <link rel="stylesheet" href="{{ asset('style.css') }}">
 
 </head>
 
@@ -272,7 +272,7 @@
                     <div class="projects-section">
                         <div>
                             <div class="project-card">
-                                <img src="{{ url('images/millow.png') }}" alt="Millow">
+                                <img src="{{ asset('images/millow.png') }}" alt="Millow">
                                 <div class="project-overlay">
                                     <a href="https://github.com/shivansh324/Millow" target="_blank"
                                         class="project-link">
