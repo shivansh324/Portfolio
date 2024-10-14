e8373507ec6427044cfae8aee6edb234ee010cf5
Shivansh Agarwal <shivansha56@gmail.com>
2024-10-14 22:36:10 +0530

    Added Fevicon

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Portfolio Website</title>
-    <link rel="icon" type="image/x-icon" href="{{ asset('images/profile.jpg') }}">
+    <link rel="icon" type="image/x-icon" href="https://shivanshagarwal-portfolio.vercel.app/images/profile.jpg">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
